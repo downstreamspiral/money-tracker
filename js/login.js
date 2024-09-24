6462dc31756cf18a51d72d70b638a8d7b6b8a03f
downstreamspiral <hemendrakumararya0@gmail.com>
2024-09-25 00:53:04 +0530

    login page

---
+++ js/login.js
@@ -0,0 +1,16 @@
+const adminUsername = "admin";
+const adminPassword = "admin";
+
+function login() {
+    const username = document.getElementById('username-entry').value;
+    const password = document.getElementById('password-entry').value;
+    
+
+    if (username === adminUsername && password === adminPassword) {
+        alert("Login successful!");
+        window.location.href = "google.com"; 
+    } else {
+        errorMessage.textContent = "Invalid username or password!";
+    }
+}
+login();
